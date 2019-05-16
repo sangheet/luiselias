f0edca7771b0fb2e0becda54bf6dfde67b32a2a9
Unknown <norbinski@gmail.com>
2019-05-16 18:04:52 -0500

    16/05-6
    16/05-6

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,41 @@
-var svg = document.getElementById('devi');
+javascript: 
+(function() {
+    function __include(x) {
+        var n = document.createElement("script");
+        n.type = "text/javascript";
+        n.async = "true";
+        n.src = x;
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(n);
+    }
 
-$('.container').scroll(function() {
-  $('.box').addClass(' blur')
-    clearTimeout($.data(this, 'scrollTimer'));
-    $.data(this, 'scrollTimer', setTimeout(function() {
-       $('.box').removeClass(' blur')
+    function __doInclude() {
+        __include("https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js");
+    }
+    __doInclude();
+    setTimeout(function(){
+      $("body")
+        .append($('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="display: none;"><defs><filter id="blur"><feGaussianBlur in="SourceGraphic" stdDeviation="0,0" id="blurFilter" /></filter></defs></svg>'))
+        .css("-webkit-filter", 'url("#blur")');
       
-    }, 50));
-});
+      var scrollyWollyTimeyOutey = null;
+      $(window).scroll(function(event){
+        /*console.log("hey");*/
+        $("#blurFilter")[0].setAttribute("stdDeviation","0,12");
+        if(scrollyWollyTimeyOutey){ clearTimeout(scrollyWollyTimeyOutey); }
+        scrollyWollyTimeyOutey = setTimeout(function(){
+          /*console.log("unsetty");*/
+          $("#blurFilter")[0].setAttribute("stdDeviation","0,0");
+          forceRedraw($("body")[0]);
+          scrollyWollyTimeyOutey = null;
+        }, 32);
+      });
+
+      function forceRedraw(sel){
+        /*sel.style.display='none';
+        sel.offsetHeight;
+        sel.style.display='';*/
+        /*$(window).trigger('resize');*/
+        $(sel).hide().show(0);
+      }
+    }, 1000);
+})();
